658fe6db81becbaad6d7145f47bb62de87afd0ed
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-11-26 22:13:32 +0500

    Task6

---
+++ Tyuiu.BabaiKV.Sprint5.Task6.V17.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BabaiKV.Sprint5.Task6.V17.Lib
+{
+    public class DataService : ISprint5Task6V17
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int c = 0;
+            int s = 0;// podryad
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        
+                        if (line[i] == ' ')
+                        {
+                            s++;  
+                        }
+                        else
+                        {
+                            
+                            if (s > 1)
+                            {
+                                c++;
+                            }
+                            s = 0; 
+                        }
+                    }
+ 
+                }
+
+            }
+            return c;
+        }
+    }
+}
---
+++ Tyuiu.BabaiKV.Sprint5.Task6.V17/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.BabaiKV.Sprint5.Task6.V17.Lib;
+namespace Tyuiu.BabaiKV.Sprint5.Task6.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Бабай К.В. | СМАРТб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #17                                                              *");
+            Console.WriteLine("* Выполнила: Бабай К.В.  | СМАРТб-24-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V17.txt";
+            Console.WriteLine("Данные в файле " + path);
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(" Двойных пробелов " + res);
+            Console.ReadKey();
+        }
+    }
+}
